67d655b33e1774d7e6ed6b00e2acc7e0438228eb
Carlos Alberto Hernández Arrieta <merecmoney3@gmail.com>
2020-07-16 23:08:59 -0500

    Agregamos Card Container con la información de los hospitales

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 import Test from "./components/Test";
+import CardContainer from "./components/CardContainer";
 import Card from "./components/Card";
 
 function App() {
+  const [data, setData] = useState(new Map());
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://datos.cdmx.gob.mx/api/records/1.0/search/?dataset=capacidad-hospitalaria&q=&rows=100&sort=fecha&facet=fecha&facet=nombre_hospital&facet=institucion&facet=estatus_capacidad_hospitalaria&facet=estatus_capacidad_uci"
+        );
+
+        const data = response.data;
+        const hospitales = data.records;
+
+        let HospitalesMap = new Map();
+        for (const record of hospitales) {
+          let nombreHospital = record.fields.nombre_hospital;
+          let informacionHospital = record.fields;
+          if (!HospitalesMap.get(nombreHospital)) {
+            HospitalesMap.set(nombreHospital, informacionHospital);
+          }
+        }
+
+        setData((prevState) => {
+          let newState = prevState;
+          newState = HospitalesMap;
+          return newState;
+        });
+      } catch (error) {
+        console.log("ERROR RECUPERANDO DATOS");
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(data)
   return (
     <>
-      {/* <Card></Card> */}
-      <Test></Test>
+      <CardContainer
+        data={Array.from(data.values())}
+      />
+      {/* <Test></Test> */}
     </>
   );
 }
--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,23 +1,37 @@
 import React, { useEffect } from "react";
 import Card from "./Card";
+import shortid from 'shortid';
+
 
 const CardContainer = ({ data }) => {
-  useEffect(() => {
-    renderHospitals(data);
-  }, [data]);
+  const cards = [];
 
-  const renderHospitals = (data) => {
-    if (data) {
-      const cards = [];
-      for (const hospital of data) {
-        cards.push();
-      }
-    } else {
-      return <p>Loading</p>;
+  if (data) {
+    for (const hospital of data) {
+      cards.push(
+          {
+            name : hospital.nombre_hospital,
+            distance: 159,
+            occupation: hospital.estatus_capacidad_hospitalaria,
+            address: "Hola",
+            image: "https://via.placeholder.com/150"
+          }
+      );
     }
-  };
+  } else {
+    return <p>Loading</p>;
+  }
 
-  return <></>;
+  return <>
+    {
+      cards.map(card => (
+        <Card
+          key={shortid.generate()}
+          hospital={card}
+        />
+      ))
+    }
+  </>;
 };
 
 export default CardContainer;
